5d1e2b8a9bcfd4df43026dfe2dc960fc952b9b6e
kylestech95 <kylestech95@outlook.com>
2024-03-25 07:01:51 -0400

    test: insert notes

--- notes.sh
+++ notes.sh
@@ -6,3 +6,10 @@ PSQL="psql -X --username=kylestech95 --dbname=notes --tuples-only -c"
 # echo $($PSQL "alter sequence notepad_id_seq restart with 1;truncate notepad;")
 
 echo -e "\n~~~ Welcome to Notepad ~~~\n"
+sleep 1
+echo -e "\nEnter notes\n"
+
+read INPUT
+INSERT_NOTES=$($PSQL "INSERT INTO notepad(notes) values('$INPUT');")
+
+echo $INSERT_NOTES
