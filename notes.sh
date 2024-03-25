50d0663f2f1000d8b85f4217c5478f677564dea5
kylestech95 <kylestech95@outlook.com>
2024-03-25 11:10:33 -0400

    update

--- notes.sh
+++ notes.sh
@@ -3,7 +3,7 @@
 # Plug in database
 PSQL="psql -X --username=kylestech95 --dbname=notes --tuples-only -c"
 # truncate data
-# echo $($PSQL "alter sequence notepad_id_seq restart with 1;truncate notepad;")
+echo $($PSQL "alter sequence notepad_id_seq restart with 1;truncate notepad;")
 
 echo -e "\n~~~ Welcome to Notepad ~~~\n"
 sleep 1
